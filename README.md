06412f4dec306452eabe9dd359ef750c3943cfc6
matla <matla@matla.cz>
2019-01-07 15:05:47 +0100

    implementace

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 phpstansc
 =========
-<description>
+Slouží k podstrčení phpStanu phpStormu jako phpcs
+
+![example](resources/readmeImg/noticeExample.png)
+
+## Instalace
+
+Do composer.jsom přidej 
+```json5
+"repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/matlad/phpstansc"
+        }
+    ],
+```
+a pak už jen
+```bash
+composer require matla/phpstansc 
+```
+
+## Použití 
+### Nastavení phpStormu
+File->Settings->Language & Frameworks -> PHP -> Quality Tools -> Code Sniffer
+a nastavit cestu k vendor/matla/phpstancs/bin/phpcs  
+![img](resources/readmeImg/setPhpStorm1.png)
+taky je potřeba mýt povolenou inspekci od phpcs
+![img2](resources/readmeImg/setPhpStorm2.png)
 
 ## Authors
 * Adam Mátl <code@matla.cz>
---
+++ src/CsLikeFormatter.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @project  phpstansc
+ * @author   Adam Mátl <matla@matla.cz>
+ * @date     6.1.19
+ * @encoding UTF-8
+ */
+declare(strict_types=1);
+
+namespace matla\phpstansc;
+
+use PHPStan\Command\AnalysisResult;
+use PHPStan\Command\ErrorFormatter\ErrorFormatter;
+use PHPStan\File\RelativePathHelper;
+use Symfony\Component\Console\Style\OutputStyle;
+
+
+/**
+ * Class CsLikeFormatter
+ *
+ * @package Phpstansc
+ */
+class CsLikeFormatter implements ErrorFormatter
+{
+    /** @var RelativePathHelper */
+    private $relativePathHelper;
+
+    public function __construct(RelativePathHelper $relativePathHelper)
+    {
+        $this->relativePathHelper = $relativePathHelper;
+    }
+
+    /**
+     * Formats the errors and outputs them to the console.
+     *
+     * @param \PHPStan\Command\AnalysisResult              $analysisResult
+     * @param \Symfony\Component\Console\Style\OutputStyle $style
+     *
+     * @return int Error code.
+     */
+    public function formatErrors(
+        AnalysisResult $analysisResult,
+        \Symfony\Component\Console\Style\OutputStyle $style
+    ): int {
+        $style->writeln('<?xml version="1.0" encoding="UTF-8"?>');
+        $style->writeln('<phpcs version="3.4.0">');
+
+
+        foreach ($this->groupByFile($analysisResult) as $relativeFilePath => $errors) {
+            $style->writeln(
+                sprintf(
+                    '<file name="%s" errors="' . count($errors) . '" warnings="0" fixable="1">',
+                    $this->escape($relativeFilePath)
+                )
+            );
+
+            /**
+             * @var \PHPStan\Analyser\Error[] $errors
+             */
+            foreach ($errors as $error) {
+                $style->writeln(
+                    sprintf(
+                        '    <error ' .
+                        'line="%d" ' .
+                        'column="1" ' .
+                        'severity="5"  ' .
+                        'fixable="1">phpstan: %s',
+                        $error->getLine(),
+                        $this->escape($error->getMessage())
+                    ) . '</error>',
+                    OutputStyle::OUTPUT_RAW
+                );
+            }
+            $style->writeln('</file>');
+        }
+
+        $style->writeln('</phpcs>');
+
+        return $analysisResult->hasErrors() ? 1 : 0;
+    }
+
+    /**
+     * Escapes values for using in XML
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    protected function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
+    }
+
+    /**
+     * Group errors by file
+     *
+     * @param AnalysisResult $analysisResult
+     *
+     * @return array<string, array> Array that have as key the relative path of file
+     *                              and as value an array with occured errors.
+     */
+    private function groupByFile(AnalysisResult $analysisResult): array
+    {
+        $files = [];
+
+        /** @var \PHPStan\Analyser\Error $fileSpecificError */
+        foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+            $relativeFilePath = $this->relativePathHelper->getRelativePath(
+                $fileSpecificError->getFile()
+            );
+
+            $files[$relativeFilePath][] = $fileSpecificError;
+        }
+
+        return $files;
+    }
+}
+
+// CsLikeFormatter.php End
